e9c9b4934838de61c80842a0e46a6cfc0fc95aeb
Sujeram <sujetha1144@gmail.com>
2026-07-13 03:12:04 +0530

    Add URL redirection endpoint

--- src/main/java/com/example/urlshortener/controller/UrlController.java
+++ src/main/java/com/example/urlshortener/controller/UrlController.java
@@ -4,6 +4,8 @@ import com.example.urlshortener.dto.UrlRequest;
 import com.example.urlshortener.dto.UrlResponse;
 import com.example.urlshortener.service.UrlService;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/")
@@ -18,6 +20,15 @@ public class UrlController {
     public UrlResponse shortenUrl(@RequestBody UrlRequest request) {
 
         return service.shortenUrl(request);
+
+    }
+    @GetMapping("/{shortCode}")
+    public void redirect(@PathVariable String shortCode,
+                     HttpServletResponse response) throws IOException {
+
+    String originalUrl = service.getOriginalUrl(shortCode);
+
+    response.sendRedirect(originalUrl);
 
     }
 
